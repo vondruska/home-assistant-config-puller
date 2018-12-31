6d04eb881af28776cd217e324a0511ea0d94b474
Steven Vondruska <vondruska@gmail.com>
2018-12-30 20:28:25 -0500

    Fix envvar for home assistant URL

--- scripts/hook.sh
+++ scripts/hook.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 git -C /homeassistant pull origin master
-curl -X POST "$HOME_ASSiSTANT_URL/api/services/homeassistant/restart"
+curl -X POST "$HOME_ASSISTANT_URL/api/services/homeassistant/restart"
